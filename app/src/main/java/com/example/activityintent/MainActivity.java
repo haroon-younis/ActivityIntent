2b5b7a76f1aea4d7aab532ca2ac60bfb252e1c45
Younis <c3519369@jg232-m09.aet.leedsbeckett.ac.uk>
2020-03-05 12:30:51 +0000

    Finished TwoActivites exercise

--- app/src/main/java/com/example/activityintent/MainActivity.java
+++ app/src/main/java/com/example/activityintent/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.activityintent;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -34,30 +32,8 @@ public class MainActivity extends AppCompatActivity {
         mMessageEditText = findViewById(R.id.editText_main);
         mReplyHeadTextView = findViewById(R.id.text_header_reply);
         mReplyTextView = findViewById(R.id.text_message_reply);
-
-        if(savedInstanceState != null) {
-            boolean isVisible = savedInstanceState.getBoolean("reply_visible");
-
-            if(isVisible) {
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-                mReplyHeadTextView.setText(savedInstanceState.getString("reply_text"));
-                mReplyHeadTextView.setVisibility(View.VISIBLE);
-            }
-        }
-
-        Log.d(LOG_TAG, "--------");
-        Log.d(LOG_TAG, "onCreate");
     }
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        if(mReplyHeadTextView.getVisibility() == View.VISIBLE) {
-            outState.putBoolean("reply_visible", true);
-            outState.putString("reply_text", mReplyTextView.getText().toString());
-        }
-    }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button Clicked");
@@ -85,41 +61,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_TAG, "onStart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(LOG_TAG, "onPause");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(LOG_TAG, "onRestart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(LOG_TAG, "onResume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(LOG_TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(LOG_TAG, "onDestroy");
     }
 }
--- app/src/main/java/com/example/activityintent/SecondActivity.java
+++ app/src/main/java/com/example/activityintent/SecondActivity.java
@@ -23,8 +23,6 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Log.d(LOG_TAG, "onCreate");
-
         mReply = findViewById(R.id.editText_second);
 
         Intent intent = getIntent();
@@ -50,40 +48,5 @@ public class SecondActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_TAG, "onStart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(LOG_TAG, "onPause");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(LOG_TAG, "onRestart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(LOG_TAG, "onResume");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(LOG_TAG, "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(LOG_TAG, "onDestroy");
-    }
 
 }
